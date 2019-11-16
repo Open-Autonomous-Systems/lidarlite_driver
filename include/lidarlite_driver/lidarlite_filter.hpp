491a039978c834bbde61af4809034ff547cc6fac
mithunvarma <diddi.m@husky.neu.edu>
2019-11-15 20:37:34 -0500

    wip:filter

---
+++ include/lidarlite_driver/lidarlite_filter.hpp
@@ -0,0 +1,65 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * MIT License                                                             *
+ * @author      Mithun Diddi <diddi.m@husky.neu.edu>                       *
+ * @maintainer  Mithun Diddi <diddi.m@husky.neu.edu>                       *
+ * @website    https://web.northeastern.edu/fieldrobotics                  *
+ * @copyright (c) 2019, Northeastern University Field Robotics Lab(NEUFRL),*
+ *             All rights reserved.                                        *
+ *                                                                         *
+ * Permission is hereby granted, free of charge, to any person obtaining   *
+ * copy of this software and associated documentation files                *
+ * (the "Software"), to deal in the Software without restriction,          *
+ * including *without limitation the rights to use, copy, modify, merge,   *
+ * publish, distribute, sublicense, and/or sell copies of the Software,    *
+ * and to permit persons to whom the Software is furnished to do so,       *
+ * subject to the following conditions:                                    *
+ *                                                                         *
+ * The above copyright notice and this permission notice shall be included *
+ * in all copies or substantial portions of the Software.                  *
+ *                                                                         *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS *
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF              *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  *
+ *                                                                         *
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR        *
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF          *
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH *
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.              *
+ * This code is wrapped using JHLidarLite github repository Licensed Under *
+ * MIT License, located at include/JHLidarLite/ locally. the original copy *
+ * of license can be obtained \                                            *
+ * from https://github.com/jetsonhacks/JHLidarLite.git                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ 
+#ifndef LIDARLITE_FILTER_H  
+#define LIDARLITE_FILTER_H 
+/*
+// System includes
+#include "unistd.h"
+#include <iostream>
+#include <string>
+// boost includes
+#include <boost/thread.hpp>
+// ROS includes
+#include <ros/ros.h>
+
+*/
+namespace lldriver_ns
+{
+	class Lidarlite_filter
+	{
+		public:
+			Lidarlite_filter();
+			virtual ~Lidarlite_filter();
+
+			//void measurementloop();
+		private:
+			//std::string robot_ns_, tf_prefix_, sensor_location_;
+			//double lidar_rate_;
+			//bool readparams();
+			//void ros_reg_topics();
+    	    //LidarLite *lidarLite_;
+        	//std::unique_ptr<boost::thread> MeasurementThread_;
+    };	//Lidarlite_filter class
+} // lldriver_ns
+#endif // LIDARLITE_FILTER_H
